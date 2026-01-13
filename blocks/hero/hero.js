a6a02d1dacbfe22eba2415097a2102aa6ec92c38
bluedeepart <deepti.rathor46@gmail.com>
2026-01-13 21:40:59 +0530

    updated default blocks

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -3,7 +3,7 @@ export default async function decorate(block) {
   const heroSection = block.closest('.section');
   if (heroImage?.src) {
     heroSection.style.backgroundImage = `url(${heroImage?.src})`;
-    heroSection.classList.add('text-white');
+    heroSection.classList.add('text-white', 'hero-bg');
     heroImage.closest('p').remove();
   }
 }
